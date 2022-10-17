3ad11bebffbe075b2e3f4219acf954b1c8905308
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-10-17 15:29:44 -0600

    make template mine
    muahahahahahahah

---
+++ src/main/java/io/github/stereo528/hud_tweaks/HUDTweaksClient.java
@@ -0,0 +1,17 @@
+package io.github.stereo528.hud_tweaks;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HUDTweaksClient implements ClientModInitializer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger("HUDTweaks");
+
+	@Override
+	public void onInitializeClient(ModContainer mod) {
+
+	}
+}
--- src/main/java/io/github/stereo528/hud_tweaks/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/stereo528/hud_tweaks/mixin/TitleScreenMixin.java
@@ -1,7 +1,7 @@
-package com.example.example_mod.mixin;
+package io.github.stereo528.hud_tweaks.mixin;
 
-import com.example.example_mod.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import io.github.stereo528.hud_tweaks.HUDTweaksClient;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
 	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		HUDTweaksClient.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
