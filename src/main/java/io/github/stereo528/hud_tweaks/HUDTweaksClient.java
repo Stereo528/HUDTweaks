efd86a183948261ed792d834b5c983f2fb7833e9
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-10-17 22:35:04 -0600

    ok it launches now

--- src/main/java/io/github/stereo528/hud_tweaks/HUDTweaksClient.java
+++ src/main/java/io/github/stereo528/hud_tweaks/HUDTweaksClient.java
@@ -1,17 +1,18 @@
 package io.github.stereo528.hud_tweaks;
 
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HUDTweaksClient implements ClientModInitializer {
 
+	public static final String MODID = "hud_tweaks";
 	public static final Logger LOGGER = LoggerFactory.getLogger("HUDTweaks");
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-
+		LOGGER.info(MODID + "AAAAAAAAAAAAAAAAAAAAAAA");
 	}
+
 }
