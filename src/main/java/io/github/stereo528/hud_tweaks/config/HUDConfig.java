5d21fe1cf150d026189a16b0a86b16fa817f5154
Stereo528 <52116771+Stereo528@users.noreply.github.com>
2022-10-26 10:34:58 -0600

    some funni comments

--- src/main/java/io/github/stereo528/hud_tweaks/config/HUDConfig.java
+++ src/main/java/io/github/stereo528/hud_tweaks/config/HUDConfig.java
@@ -7,14 +7,35 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "hudtweaks")
 public class HUDConfig implements ConfigData {
+	@Comment("Shows your FPS")
+    @ConfigEntry.Gui.Tooltip
 	public boolean fps = true;
+	
+	@Comment("Shows your XYZ Coordinates")
+    @ConfigEntry.Gui.Tooltip
 	public boolean coords = true;
+	
+	@Comment("Your Pos/Neg XZ Direction")
+    @ConfigEntry.Gui.Tooltip
+	public boolean facing = true;
+	
+	@Comment("Your North, East, South, West facing direction")
+    @ConfigEntry.Gui.Tooltip
+	public boolean nesw = true;
 
+	@Comment("CPU Brand/Model")
+    @ConfigEntry.Gui.Tooltip
 	public boolean gpu = false;
+	
+	@Comment("GPU Brand/Model")
+    @ConfigEntry.Gui.Tooltip
 	public boolean cpu = false;
 
-	public boolean cpu_and_gpu_on_right = true;
 
 	public boolean gpu_usage = false;
 	public boolean cpu_usage = false;
+	
+	@Comment("CPU/GPU & Usages on the top right instead of top left of the screen")
+    @ConfigEntry.Gui.Tooltip
+	public boolean cpu_and_gpu_on_right = true;
 }
